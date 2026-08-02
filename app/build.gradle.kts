621da6369e44d0029af792133f56a07a5cdc9a9d
BenEgeDeniz <benegedeniz@users.noreply.github.com>
2026-08-02 15:27:28 +0300

    build: upgrade Java toolchain to 21 to natively support F-Droid Trixie buildserver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
@@ -37,7 +37,7 @@ android {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
